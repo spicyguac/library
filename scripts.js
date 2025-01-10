c4321f5bb031cf0fccfca5f358165b5104df4a82
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2025-01-10 11:42:35 -0600

    Create skeleton functions and boilerplate html

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const shelf = document.querySelector(".shelf");
+
+
+const myLibrary = []
+
+function Book() {
+    this.title = "";
+    this.author = "";
+    this.numOfPages = 0;
+    this.readTheBook = "";
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayLibary() {
+    myLibrary.forEach(function(book){
+        
+    });
+}
