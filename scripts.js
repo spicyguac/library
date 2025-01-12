1453e21919509ebf3b548cc5b6c40bb89125a262
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2025-01-11 19:20:22 -0600

    Add button to display the add book form

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 const shelf = document.querySelector(".shelf");
 const form = document.getElementById("bookFrom");
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector(".addBook")
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    addBookToLibrary();
+
+})
+
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+})
+
 
 const myLibrary = []
 
@@ -7,27 +20,26 @@ function Book() {
     
 }
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const bookDetails = new FormData(form);
-    const addbook = new Book();
-
-    for (const [key, value] of bookDetails.entries()) {
-        addbook[key] = value;    
-    }
-
-    myLibrary.push(addbook);
-    
-    console.log(myLibrary);
-
-    form.reset();
-});
-
-
-
+//Create Book object from Form input
 function addBookToLibrary() {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
     
+        const bookDetails = new FormData(form);
+        const addbook = new Book();
+    
+        for (const [key, value] of bookDetails.entries()) {
+            addbook[key] = value;    
+        }
+    
+        myLibrary.push(addbook);
+        
+        console.log(myLibrary);
+    
+        form.reset();
+
+        dialog.close();
+    });
 }
 
 function displayLibary() {
