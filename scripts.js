e7e88b6a1c1c3d8edc54415a5c01694ee84c994a
spicyguac <200~46731258+spicyguac@users.noreply.github.com>
2025-01-13 18:16:11 -0600

    Make remove button functional to remove books

--- scripts.js
+++ scripts.js
@@ -2,20 +2,17 @@ const shelf = document.querySelector(".shelf");
 const form = document.getElementById("bookFrom");
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".addBook");
-const removeBook = document.querySelector(".removeBook");
+
 
 document.addEventListener("DOMContentLoaded", function() {
     addBookToLibrary();
     
+    //setInterval(removeBooks, 10000);
 })
 
 showButton.addEventListener("click", () => {
     dialog.showModal();
 })
-
-// removeBook.addEventListener("click", () =>{
-
-// })
 
 const myLibrary = []
 
@@ -37,21 +34,25 @@ function addBookToLibrary() {
     
         myLibrary.push(addbook);
         
-        console.log(myLibrary);
+        //console.log(myLibrary);
     
         form.reset();
 
         dialog.close();
 
         displayBook(addbook);
+
+        //removeBooks();
     });
+    
 }
 
 //Create book element to display on shelf
 function displayBook(addBook) {
         let bookOnShelf = document.createElement("div");
         bookOnShelf.classList.add("bookAdded");
-        bookOnShelf.id = myLibrary.length;
+        bookOnShelf.id = addBook.bookTitle;
+        let id = addBook.bookTitle;
         
         let bookHeight = 150/9;
         let bookWidth = 100/9;
@@ -60,18 +61,40 @@ function displayBook(addBook) {
         bookOnShelf.style.width = `${bookWidth}vh`;
         
         shelf.appendChild(bookOnShelf);
-        console.log(addBook);
+        //console.log(addBook);
 
-        let newBook = document.querySelector(".bookAdded")
+        let newBook = document.getElementById(id);
 
         let title = document.createElement("p")
-        title.textContent = addBook.bookTitle
+        title.textContent = addBook.bookTitle;
 
         newBook.appendChild(title);
 
-        let remove = document.createElement("button");
-        remove.classList.add("removeBook");
-        remove.textContent = "Remove";
+        let finished = document.createElement("button");
+        
 
-        newBook.appendChild(remove);
+        let removeButton = document.createElement("button");
+        removeButton.classList.add("removeBook");
+        removeButton.id = id;
+        removeButton.textContent = "Remove";
+
+        newBook.appendChild(removeButton);
+
+        const removeBook = document.getElementById(id);
+        removeBook.addEventListener("click", (event) => {
+            console.log(event.target.id);
+            const removeElements = document.getElementById(event.target.id)
+            removeElements.remove();
+        })
 }
+
+function removeBooks (){
+    const remove = document.querySelectorAll(".removeBook");
+    remove.forEach((book) => {
+        book.addEventListener("click", (event) => {
+            console.log(event.target.id);
+           
+        })
+    })
+}
+
